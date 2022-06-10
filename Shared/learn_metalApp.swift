e826041ef04867f1ebe8bd90d1e57bde02fa6337
yuandongming <smartdone@163.com>
2022-06-10 16:59:46 +0800

    Initial Commit

---
+++ Shared/learn_metalApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_metalApp.swift
+//  Shared
+//
+//  Created by 袁东明 on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct learn_metalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
